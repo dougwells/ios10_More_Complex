8957cf16188b456c2fbb6eee31c2fc9045caf49d
Doug Wells <dougwells@gmail.com>
2016-12-01 07:54:57 -0700

    ios10. More Complex. Tic tac. Add winner logic - WIP

--- tic-tac-toe/tic-tac-toe/ViewController.swift
+++ tic-tac-toe/tic-tac-toe/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var xWin = false
+    var oWin = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +21,40 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func anyWinner (ticTacArr:[[Int]]) {
+
+    }
+    
+
+    
+/*    func checkRows (arr:[[Int]]) {
+        for i in arr {
+            let sq1 = arr[i][0]
+            let sq2 = arr[i][1]
+            let sq3 = arr[i][2]
+            whoWins(square1: sq1, square2: sq1, square3: sq1)
+        }
+    }
+ */
+    
+    func checkCols (ticTacArr:[[Int]]) {
+        
+    }
+    
+    func checkDiag (ticTacArr:[[Int]]) {
+        
+    }
+    
+    func whoWins (square1: Int, square2: Int, square3: Int) {
+        let score = square1 + square2 + square3
+        if score == 3 {xWin = true}
+        if score == 0 {oWin = true}
+        return
+    }
+    
+    
+    
 
 
 }
