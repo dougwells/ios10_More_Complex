42f3a6f67b6e08a99443e334e6cfce22693e193a
Doug Wells <dougwells@gmail.com>
2016-12-15 13:15:09 -0700

    ios. Complex. Audio Player. Add play, pause & volume control

--- audioPlayer/audioPlayer/ViewController.swift
+++ audioPlayer/audioPlayer/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player = AVAudioPlayer()
+    @IBOutlet var slider: UISlider!
+    
+    @IBAction func play(_ sender: Any) {
+        player.play()
+    }
+    @IBAction func pause(_ sender: Any) {
+        player.pause()
+    }
+    @IBAction func sliderMoved(_ sender: Any) {
+        player.volume = slider.value
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let audioPath = Bundle.main.path(forResource: "rainbow", ofType: "mp3")
+        do {
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            
+            
+            
+        } catch {
+            
+            //process errors
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
 
