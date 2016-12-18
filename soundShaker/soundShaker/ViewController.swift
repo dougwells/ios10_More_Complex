fb905359b11ed05856d4d04f8a91a14d10e0c43a
Doug Wells <dougwells@gmail.com>
2016-12-18 08:05:56 -0700

    ios. Complex. Sound Shaker. Add user interface. All functions working.

--- soundShaker/soundShaker/ViewController.swift
+++ soundShaker/soundShaker/ViewController.swift
@@ -14,16 +14,22 @@ class ViewController: UIViewController {
     var player = AVAudioPlayer()
     var soundTrack = "howl"
     var audioPath: String = ""
+    let soundArr = ["horn", "hillbilly", "phone", "saloon", "meow", "bark", "airRaid", "jet", "howl", "whistle", "thunder"]
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        audioPath = Bundle.main.path(forResource: soundTrack, ofType: "mp3")!
+
 
     }
     
     override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
         if event?.subtype == UIEventSubtype.motionShake {
+            let i = Int(arc4random_uniform(UInt32(soundArr.count)))
+            soundTrack = soundArr[i]
+            audioPath = Bundle.main.path(forResource: soundTrack, ofType: "mp3")!
+            print("Device shaken. Soundtrack = ", soundTrack)
+            
             do {
                 try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath))
                 player.play()
